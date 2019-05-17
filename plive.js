2932655f53af1d49a0be8f36b4ae44884a7b91ca
loz <lauriej1316@gmail.com>
2019-05-17 17:20:54 +0100

    pre algomech stable - but also a mess of possible implementations

--- plive.js
+++ plive.js
@@ -4,45 +4,33 @@ var plive = plive || {};
 	
 	// TODO - webpack
 	
+	// regex to get func params in a string - /[^(\)]+(?=\))/g
+	
   /* 
   
-  maybe:
+  createGraphics approach issues:
   
-  var globalPipeline = [];	// make sure to reset on ctrlenter
+  - still doesn't solve using eg f in func args, so have to eval entire codeString every frame
   
-  v0 = () => {
-	  var obj = createGraphics(w, h);
-	  globalPipeline.push(obj);
-	  return obj;
-  }
-  
-  // in text buffer:
-  
-  v0().stroke(200).line(a, b, c, d)
-  
-  // in internal draw:
-  
-  for(gfx in globalPipeline) image(gfx, 0, 0, w, h);
-  
-  // issues:
-  
-  - if using createGraphics, might have to use beginDraw and endDraw - actually doesn't seem to be the case ?
+  - how to extend the vanilla pgraphics obj with additional funcs ?
   
   */
   
   var textArea, codeMirror, sock;
   
   var codeString = '';
+  
+  var globalPipeline = [];
 	
 	function _createGlobals() {
 		window.w = window.innerWidth;
 		window.h = window.innerHeight;
 		window.f = 0;
 		window.t = 0;
-		window.a = window.b = window.c = window.d = window.e = window.g = window.i = window.j = window.k = undefined;
+		window.a = window.b = window.c = window.d = window.e = window.g = window.i = window.j = window.k = 0;
 		for(var i = 0; i < 10; i++) {
 			window['d'+i] = function() {};
-			window['v'+i] = { id: i, pipe: [] };
+			//window['v'+i] = { id: i, pipe: [] };
 		}
 		window.mic = undefined;
 	}
@@ -88,7 +76,7 @@ var plive = plive || {};
 		
 		// utility
 		
-		window.iter = function(numTimes, action) {
+		window.many = function(numTimes, action) {
 			for(var i = 0; i < numTimes; i++) action(i);
 		}
 		
@@ -114,7 +102,9 @@ var plive = plive || {};
 	
 	function _buildChainables() {
 		
-		var p5funcs = [
+		// --- old section ---
+		
+		/*var p5funcs = [
 			'background', 'bg', 'fade', 
 			'stroke', 'noStroke', 'fill', 'noFill', 'tint', 'noTint',
 			'weight', 'strokeWeight', 'strokeCap', 'strokeJoin',
@@ -123,7 +113,7 @@ var plive = plive || {};
 			'image', 'text', 
 			'loadPixels', 'updatePixels', 'get', 'set', 'filter', 'blendMode', 'blend',
 			'rectMode', 'ellipseMode', 'imageMode',
-			'iter', 'every',
+			'many', 'every',
 		];
 		
 		for(var i = 0; i < 10; i++) {
@@ -132,10 +122,10 @@ var plive = plive || {};
 					(function(func) {
 						window['v'+i][func] = function () {
 							window[func](...arguments);
-							/*this.pipe.push({
-								func: func,
-								args: arguments
-							});*/
+							//this.pipe.push({
+							//	func: func,
+							//	args: arguments
+							//});
 							return this;
 						}.bind(window['v'+i]);
 					})(p5funcs[f]);
@@ -144,20 +134,91 @@ var plive = plive || {};
 				//	console.warn('couldnt add ' + p5funcs[f] + ' to chainable objects !');
 				//}
 			}
-		}
+		}*/
 		
 		// modify p5 api
 		
 		// set context-sensible defaults, eg image(img) same as image(img, 0, 0, w, h) 
 		
+		// --- end old section ---
+		
+		// extend p5 funcs that aren't chainable
+		// TODO - separate into simple extensions (eg tint, can loop thru) and custom eg image()
+		
+		var _image = p5.prototype.image;
+		p5.prototype.image = function (img, x, y, w, h) {
+			x = typeof x === 'undefined' ? 0 : x;
+			y = typeof y === 'undefined' ? 0 : y;
+			w = typeof w === 'undefined' ? window.w : w;
+			h = typeof h === 'undefined' ? window.h : h;
+			var image = _image.apply(this, [img, x, y, w, h]);
+			return this;
+		}
+		
+		var _tint = p5.prototype.tint;
+		p5.prototype.tint = function () {
+			_tint.apply(this, arguments);
+			return this;
+		}
+		
+		var _pop = p5.prototype.pop;
+		p5.prototype.pop = function () {
+			_pop.apply(this, arguments);
+			return this;
+		}
+		
+		var _push = p5.prototype.push;
+		p5.prototype.push = function () {
+			_push.apply(this, arguments);
+			return this;
+		}
+		
+		// aliases
+		
+		p5.prototype.bg = function () {
+			p5.prototype.background.apply(this, arguments);
+			return this;
+		}
+		
+		p5.prototype.weight = function () {
+			p5.prototype.strokeWeight.apply(this, arguments);
+			return this;
+		}
+		
+		p5.prototype.blend = function () {
+			p5.prototype.blendMode.apply(this, arguments);
+			return this;
+		}
+		
+		// new funcs
+		
+		p5.prototype.fade = function (amt) {
+			amt = typeof amt === 'undefined' ? 10 : amt;
+			p5.prototype.push.call(this);
+			p5.prototype.noStroke.call(this);
+			p5.prototype.fill.call(this, 0, amt);
+			p5.prototype.rectMode.call(this, CORNER);
+			p5.prototype.rect.call(this, 0, 0, w, h);
+			p5.prototype.pop.call(this);
+			return this;
+		}
+		
+		p5.prototype.opacity = function (amt) {
+			amt = typeof amt === 'undefined' ? 0.5 : amt;
+			this._renderer.drawingContext.globalAlpha = amt;
+			return this;
+		}
+		
 	}
 	
 	function _configureP5() {
+		
 		p5.disableFriendlyErrors = true;
 		
 		window.setup = function() {
 			var renderer = createCanvas(w, h);
 			window.mainCanvas = renderer;
+			window.mainContext = renderer.elt.getContext('2d');
 			//console.log(renderer)
 			document.documentElement.style.overflow = 'hidden';
 			background(0);
@@ -169,6 +230,16 @@ var plive = plive || {};
 			//   value: "draw_ = function() {\n background(0);\n}",
 			//   mode:  "javascript"
 			// });
+			
+			for(var i = 0; i < 10; i++) {
+				window['v'+i] = (function() {
+					var obj = createGraphics(w, h);
+					return function() {
+						globalPipeline.push(obj);
+						return obj;
+					}
+				})();
+			}
 
 			mic = new p5.AudioIn();
 
@@ -180,6 +251,17 @@ var plive = plive || {};
 			
 			f = window.frameCount;
 			t = performance.now() * 1000;
+			
+			// approach 3 - 'vi' objects are now funcs, generating a pgraphics obj that is already chainable and added to global pipeline on ctrlenter. pipeline is cleared / vi obj replaced on ctlenter
+			try {
+				eval(codeString);
+			} catch(e) {
+				// warning ! accessing global obj vij from inside private closure
+				vij.error = e.message;
+			}
+			for(i = 0; i < 4; i++) image(window['v'+i](), 0, 0, w, h);
+			//for(i = 0; i < globalPipeline.length; i++) image(globalPipeline[i], 0, 0, w, h);
+			
 			for(i = 0; i < 10; i++) {
 				
 				try {
@@ -188,6 +270,7 @@ var plive = plive || {};
 					console.log(e.message);
 				}
 				
+				// approach 1 - store each chained function call into a local pipe within each 'vi' object
 				/*push();
 				
 				for(j = 0; j < window['v'+i].pipe.length; j++) {
@@ -199,7 +282,8 @@ var plive = plive || {};
 				pop();*/
 			}
 			
-			eval(codeString);
+			// approach 2 - store codeString on ctrlenter, then eval every frame. chained functions have internal p5 side effects
+			//eval(codeString);
 		}
 		
 		window.keyPressed = function() {
@@ -219,9 +303,11 @@ var plive = plive || {};
 	}
 	
 	pl.evaluate = function(s) {
+		while(globalPipeline.length) globalPipeline.pop();
 		codeString = s;
 		return;
-		for(var i = 0; i < 10; i++) while(window['v'+i].pipe.length) window['v'+i].pipe.pop();
+		//return;	// uncomment if returning to approach 2
+		//for(var i = 0; i < 10; i++) while(window['v'+i].pipe.length) window['v'+i].pipe.pop(); // uncomment if returning to approach 1
 		// look for double newlines, only eval the current set of lines
 		// lines = txtEl.value.split(/\n\s*\n/)
 		try {
